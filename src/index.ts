663455b90b2ed491dae8aafa701b4fd1f3693e9b
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2022-06-24 21:26:55 +0300

    feat: install dependencies

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+import { env } from 'process';
+import {} from 'process';
+
+dotenv.config();
+const { PORT } = env;
+
+console.log(PORT);
+
+const a = 5;
+
+console.log(a);
